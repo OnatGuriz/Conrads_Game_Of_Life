b0018ee8ce6a69b9932ac0766ceb2e8b3aa018d4
OnatGuriz <51419540+OnatGuriz@users.noreply.github.com>
2024-09-04 15:30:23 +0200

    game logic fix
    fixed a logic bug small performance upgrade

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -2,11 +2,15 @@ extends TileMapLayer
 # The variables of the tilemaps on the atlas
 const alive_cell = Vector2i(1,1)
 const dead_cell = Vector2i(4,1)
+var cells_to_be_killed = []
+var cells_to_be_spawned = []
+var candidate_cells = []
 
 # Atlas coordinates of every alive cell on the grid
 var alive_cell_ids = []
 var time_elapsed:float = 0;
 var game_ready = false
+var alive_neighbours
 const neighbour_index = [Vector2i(-1,-1), Vector2i(0,-1), Vector2i(-1,0), Vector2i(1,-1),
 						Vector2i(-1,1), Vector2i(1,0), Vector2i(0,1), Vector2i(1,1) ]
 
@@ -20,11 +24,11 @@ func get_alive_neighbor_count(pos) -> int:
 
 #gets the dead cells that can possibly become alive with reproduction
 func get_candidate_dead_cells():
-	var candidate_cells = []
 	for pos in alive_cell_ids:
 		for vec in neighbour_index:
 			if ((pos + vec) not in  alive_cell_ids):
-				candidate_cells.append(pos+vec)
+				if (not (candidate_cells.has(pos +vec))) :
+					candidate_cells.append(pos+vec)
 	return candidate_cells
 
 func kill_cell(pos):
@@ -37,18 +41,25 @@ func spawn_cell(pos):
 
 
 func run_game():
+	candidate_cells = get_candidate_dead_cells()
 	for pos in alive_cell_ids:
+		alive_neighbours = get_alive_neighbor_count(pos)
 		#death by underpopulation
-		if(get_alive_neighbor_count(pos) < 2):
-			kill_cell(pos)
+		if(alive_neighbours < 2):
+			cells_to_be_killed.append(pos)
 		#death by overpopulation
-		elif(get_alive_neighbor_count(pos) > 3):
-			kill_cell(pos)
-	var candidates = get_candidate_dead_cells()
-	for candidate in candidates:
+		elif(alive_neighbours > 3):
+			cells_to_be_killed.append(pos)
+	for candidate in candidate_cells:
 		if(get_alive_neighbor_count(candidate) == 3):
-			spawn_cell(candidate)
-
+			cells_to_be_spawned.append(candidate)
+	for cell in cells_to_be_killed:
+		kill_cell(cell)
+	for cell in cells_to_be_spawned:
+		spawn_cell(cell)
+	cells_to_be_killed.clear()
+	cells_to_be_spawned.clear()
+	candidate_cells.clear()
 
 # Method to update cell type it will flip the cell type
 func cell_pressed(pos):
@@ -64,8 +75,9 @@ func _on_node_2d_game_ready():
 	
 # runs one step of the game every 0.1 seconds
 func _process(delta: float) -> void:
-	if(time_elapsed < 0.1):
-		time_elapsed += delta
-	if(time_elapsed > 0.1 and game_ready):
-		run_game()
-		time_elapsed = 0
+	if  game_ready:
+		if(time_elapsed < 0.5):
+			time_elapsed += delta
+		if time_elapsed > 0.5:
+			run_game()
+			time_elapsed = 0
